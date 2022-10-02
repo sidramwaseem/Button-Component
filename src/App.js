bee79806685fcbffddcd740cb0d0c1e13c62c5fa
Sidra Waseem <sidraw24@gmail.com>
2022-10-02 16:54:23 +0500

    changed bootstrap path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import Buttons from './components/Buttons/Buttons.jsx';
-import Nav from './components/Nav/Nav.jsx';
+import React from "react";
+import Buttons from "./components/Buttons/Buttons.jsx";
+import Nav from "./components/Nav/Nav.jsx";
 
 function App() {
   return (
-    <div>
-    <Nav/>
-      <Buttons/>
-    </div>
-  )
+    <section className="container">
+      <div className="row">
+        <div className="col-2 col-sm-2 col-lg-2">
+          <Nav />
+        </div>
+        <div className="col-10 col-sm-10 col-lg-10">
+          <Buttons />
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;

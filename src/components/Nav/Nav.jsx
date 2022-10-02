d4959bc00e04c053c9c6f78c267cc6abb5679398
Sidra Waseem <sidraw24@gmail.com>
2022-10-02 16:54:30 +0500

    update nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 function Nav() {
   return (
-    <div>
-      nav
-    </div>
+    <ul>
+      <li>Colors</li>
+      <li>Typography</li>
+      <li>Spaces</li>
+      <li>Buttons</li>
+      <li>Inputs</li>
+      <li>Grid</li>
+    </ul>
   )
 }
 

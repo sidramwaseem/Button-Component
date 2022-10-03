59f95326ff6fa007bd7687ef62f23ba47a88989e
Sidra Waseem <sidraw24@gmail.com>
2022-10-03 12:36:00 +0500

    update app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 import Buttons from "./components/Buttons/Buttons.jsx";
 import Nav from "./components/Nav/Nav.jsx";
+import './App.css';
 
 function App() {
   return (
     <section className="container">
-      <div className="row">
-        <div className="col-2 col-sm-2 col-lg-2">
-          <Nav />
-        </div>
-        <div className="col-10 col-sm-10 col-lg-10">
-          <Buttons />
-        </div>
-      </div>
+      <Nav />
+
+      <Buttons />
     </section>
   );
 }

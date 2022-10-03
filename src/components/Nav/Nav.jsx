ae53595fc90a043bdc40670ea5e0a471e1671bc5
Sidra Waseem <sidraw24@gmail.com>
2022-10-03 12:36:17 +0500

    update nav.jsx

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
+import './Nav.css';
 
 function Nav() {
   return (
-    <ul>
-      <li>Colors</li>
-      <li>Typography</li>
-      <li>Spaces</li>
-      <li>Buttons</li>
-      <li>Inputs</li>
-      <li>Grid</li>
-    </ul>
-  )
+    <nav className="nav">
+    <h5><span className="red">Dev</span>Challenges.io</h5>
+      <ul className="nav--list">
+        <li>Colors</li>
+        <li>Typography</li>
+        <li>Spaces</li>
+        <li>Buttons</li>
+        <li>Inputs</li>
+        <li>Grid</li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
